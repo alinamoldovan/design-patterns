f1e780be873398e77b0646e88ef01dabbc620450
Alina <alina.moldovan12@gmail.com>
2025-06-09 17:10:38 +0300

    concrete implementation

---
+++ creational/factory-method/HtmlButton.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author alina
+ */
+public class HtmlButton implements Button {
+
+    @Override
+    public void render() {
+        System.out.println("<button>HTML Button</button>");
+    }
+
+    @Override
+    public void onClick() {
+        System.out.println("HTML Button clicked!");
+    }
+
+}
